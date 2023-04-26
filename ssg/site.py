591b89d3782fcb062f91599324ae2f11c347bd92
Marc Kessler <marc.kessler@sas.com>
2023-04-26 22:38:55 +0000

    add Parser subclasses

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import sys
 
 class Site:
 
@@ -30,5 +31,8 @@ class Site:
         if parser is not None:
             parser.parse (path, self.source, self.dest)
         else:
-            print("not implemented")
+            self.error ("No parser for the {} extension, file skipped!".format (path.suffix))
 
+    @staticmethod
+    def error (message):
+        sys.stderr.write ("\x1b[1;31m{}\n".format (message))
